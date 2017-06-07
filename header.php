7069f2d3cc23f3e608d1d16e631cf9071f77861c
1459044 <1459044@itec.hcmus.edu.vn>
2017-06-07 13:29:27 +0700

    Add files via upload
    Week 3 Nguyễn Nhật Quang

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+
+	<div id="logo"><img src="images/logo1.jpg" width="284"  height="96" alt="" /></div>
+	<div align="right" id="banner">
+	<!--<div  id="pay" class="p">Thẻ ngân hàng</div>
+	<div  id="truck" class="p">Vận chuyển</div>
+	<div  id="return" class="p">Không hoàn trả</div>
+	<div  id="phone" class="p">Liên hệ</div>
+	<div  id="lock" class="p">Bảo mật</div>!-->
+	<br><br><label>Search</label>
+	<input type="text" name="txtSearchText"  id="text" value="" />
+	<input type="submit" name="btnSearch" value="Tìm Kiếm" id="button" />
+	</div>
+<div class="clear"></div>
