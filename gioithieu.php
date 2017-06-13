b1dfd32db05b96c66c19c597a99b8ae57c19cfbf
1459032 <1459032@itec.hcmus.edu.vn>
2017-06-13 08:40:05 +0700

    Map va Gioi thieu 1459032

---
+++ gioithieu.php
@@ -0,0 +1,35 @@
+<h3>Thông Tin Giới Thiệu</h3>
+<div id="gioithieu">
+
+<img  class="lienhe" src="images/2014101318304417.jpg"  width="400"  id="image" alt="" />
+
+<p class="tintuc">
+Để mua một chiếc máy tính, khách hàng thường gặp không ít khó khăn khi phải lựa chọn nơi thực sự uy tín, chất lượng cùng chế độ hậu mãi tốt, trước hàng trăm doanh nghiệp đã và đang tham gia thị trường bán lẻ các mặt hàng công nghệ như hiện nay. Việc đặt niềm tin đúng chỗ sẽ mang lại cho khách hàng một sản phẩm ưng ý về mọi mặt. Một trong những nhà bán lẻ máy tính và điện thoại di động uy tín hàng đầu tại Hà Nội phải kể đến là Joli Bobi.</p>
+
+
+<span class="style">Thương hiệu uy tín</span>
+<p class="tintuc">
+Project D được thành lập năm 2017 – dịch vụ tiêu biểu, nhà bán lẻ hàng đầu do bộ công thương hiệp hội doanh nghiệp vừa và nhỏ trao tặng….
+</p>
+<p class="tintuc">
+Chúng tôi tự hào giới thiệu cho mọi người về các sản phẩm của thương hiệu nổi tiếng như: Apple,Oppo NOKIA, SAMSUNG…
+</p>
+<span class="style">Sản phẩm tốt -  Giá cả hợp lý</span>
+<p class="tintuc">
+Hiện tại Project D có các mặt hàng CNTT như: máy tính, linh kiện, thiết bị điện tử và điện thoại di dộng… với thương hiệu uy tín, sản phẩm tốt, giá hợp lý đã giúp Joli Bobi trở thành một trong những nhà bán lẻ hàng đầu Việt Nam, được hàng nghìn người đến tham quan và mua sắm mỗi ngày.</p>
+<p class="tintuc">
+Song song với việc thực hiện các chương trình khuyến mãi hấp dẫn, Project D đã chủ động đàm phán với các nhà cung cấp, các đối tác nhằm có những mức hỗ trợ giảm giá tối đa, giúp khách hàng mua sắm sao cho hiệu quả nhất. Vì vậy khi khách hàng đến với chúng tôi vừa có cơ hội sở hữu những sản phẩm chính hãng với chất lượng vượt trội của các thương hiệu nổi tiếng vừa được trải nghiệm dịch vụ chu đáo, tận tình của nhà bán lẻ hàng đầu mà không phải lo về giá cả.</p>
+
+<span class="style">Chế độ hậu mãi tốt</span>
+<p class="tintuc">
+Khách hàng khó có thể kiểm tra chất lượng máy tính khi mua tại cửa hàng, do đó đa số khách hàng đặt quan tâm nhiều đến chế độ bảo hành của nhà bán lẻ. Với quyết tâm đảm bảo quyền lợi tối đa cho người tiêu dùng, Chúng tôi đã xây dựng và cho ra đời chế độ bảo hành máy tính chỉ trong thời gian 4 giờ và dịch vụ bảo hành mở rộng, đảm bảo mang lại sự nhanh chóng, tiện lợi và an tâm tuyệt đối cho khách hàng khi mua sắm sản phẩm và sử dụng dịch vụ bảo hành, sửa chữa tại đây. Ngoài ra, Project D cam kết sẽ đổi mới cho những sản phẩm được bảo hành hơn 4 giờ.</p>
+<p class="tintuc">
+Đồng thời với đội ngũ nhân viên chuyện nghiệp, phong cách phục vụ tận tình , ân cần và chu đáo, sẽ mang lại niềm tin tuyệt đối cho khách hàng khi đến với thương hiệu máy tính nổi tiếng.</p>
+<p class="tintuc">
+Với phương châm luôn đồng hành cùng khách hàng trong thời kỳ kinh tế khó khăn như hiện nay, chúng tôi quyết định giảm lợi nhuận của chính mình để xây dựng cho khách hàng ngày càng nhiều chương trình khuyến mãi hấp dẫn, tiết kiệm được khoảng chi phí lớn trong việc mua sắm các thiết bị CNTT.</p>
+<p class="tintuc">
+Tất cả những chương trình khuyến mãi thiết thực đem lại hiệu quả đã ấp dẫn và thu hút được một lượng lớn khách hàng mỗi khi triển khai. Đa số các chương trình khuyến mãi là giảm giá, bốc thăm mua giá rẻ, quà tặng hấp dẫn… tạo cho khách hàng nhiều cơ hội sở hữu sản phẩm của Joli Bobi với giá rẻ nhất.</p>
+
+<span class="style">Project D cam kết: Tất cả vì lợi ích của khách hàng.</span>
+</div>
+<div class="clear"></div>
---
+++ map.php
@@ -0,0 +1,6 @@
+<h3>Bản Đồ</h3>
+
+	 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6342665428256!2d106.67983811508434!3d10.762644362390251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1c06f4e1dd%3A0x43900f1d4539a3d!2sUniversity+of+Science%2C+Downtown+Campus!5e0!3m2!1sen!2s!4v1495648815843" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					    <br />
+				      <small>Xem <a href="https://www.google.com/maps/place/University+of+Science,+Downtown+Campus/@10.7626444,106.6798381,17z/data=!3m1!4b1!4m5!3m4!1s0x31752f1c06f4e1dd:0x43900f1d4539a3d!8m2!3d10.7626391!4d106.6820268">TP HCM - Việt Nam</a> ở bản đồ lớn hơn</small><br />
+				
